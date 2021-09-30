ab14d9484852b0ff689c635714a1d6155d06b387
chistle <46581207+chistle@users.noreply.github.com>
2021-10-01 02:57:39 +0300

    Added random AI player and Player class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import copy
+import random
 
 
 class GameBoard(object):
@@ -29,6 +30,7 @@ class GameBoard(object):
             if not b.is_destroyed():
                 return False
         return True
+
 
 class Shot(object):
     def __init__(self, location, is_hit):
@@ -69,6 +71,14 @@ class Battleship(object):
     def is_destroyed(self):
         return all(self.hits)
 
+
+class Player(object):
+
+    def __init__(self, name, shot_f):
+        self.name = name
+        self.shot_f = shot_f
+
+
 def render(game_board, show_battleships=False):
     header = "+" + "-" * game_board.width + "+"
     print(header)
@@ -100,6 +110,22 @@ def render(game_board, show_battleships=False):
     print(header)
 
 
+def get_random_ai_shot(game_board):
+    x = random.randint(0, game_board.width - 1)
+    y = random.randint(0, game_board.height - 1)
+    return (x, y)
+
+
+def get_human_shot(game_board):
+    inp = input("Where do you want to shoot?\n")
+    # TODO: deal with invalid input
+    xstr, ystr = inp.split(",")
+    x = int(xstr)
+    y = int(ystr)
+
+    return (x, y)
+
+
 if __name__ == "__main__":
     battleships = [
         Battleship.build((1, 1), 2, "N"),
@@ -112,9 +138,9 @@ if __name__ == "__main__":
         GameBoard(copy.deepcopy(battleships), 10, 10)
     ]
 
-    player_names = [
-        "Player 1",
-        "Player 2"
+    players = [
+        Player("Kiryl", get_human_shot),
+        Player("AI", get_random_ai_shot)
     ]
 
     offencive_idx = 0
@@ -124,21 +150,17 @@ if __name__ == "__main__":
         defensive_idx = (offencive_idx + 1) % 2
 
         defensive_board = game_boards[defensive_idx]
+        offensive_player = players[offencive_idx]
 
-        print("%s YOUR TURN!" % player_names[offencive_idx])
-        inp = input("Where do you want to shoot?\n")
-        # TODO: deal with invalid input
-        xstr, ystr = inp.split(",")
-        x = int(xstr)
-        y = int(ystr)
+        print("%s YOUR TURN!" % offensive_player.name)
+        shot_location = offensive_player.shot_f(defensive_board)
 
-        defensive_board.take_shot((x, y))
+        defensive_board.take_shot(shot_location)
         render(defensive_board)
 
         if defensive_board.is_game_over():
-            print("%s WINS!" % player_names[offencive_idx])
+            print("%s WINS!" % offensive_player.name)
             break
 
         # Offensive player becomes the previous defensive one
         offencive_idx = defensive_idx
-
